70b6fe2bc4c55415a3cf43f7a486cd757640294b
ZZM <1010798057@qq.com>
2019-10-26 23:12:36 -0700

    week6

---
+++ code/dynamiclib/base.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void show(int *a,int n)
+{
+int i;
+for(i=0;i<n;i++)
+printf("%4d :%4d\n",i,a[i]);
+}
+void initarray(int *a,int n)
+{}
---
+++ code/dynamiclib/dylib.h
@@ -0,0 +1,5 @@
+int max(int *,int);
+int sum(int *,int);
+
+void show(int *,int);
+void init(int *,int);
