513d69d5676d42b73061c0dd932e385e48d7a891
ZZM <1010798057@qq.com>
2019-11-25 22:26:49 -0800

    week12

---
+++ week12/day2/my.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+
