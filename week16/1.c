bfb9bb3b7d880a550553772d4dae7f37c98b7aae
ZZM <1010798057@qq.com>
2019-12-22 17:14:55 -0800

    week16

---
+++ week16/1.c
@@ -0,0 +1,35 @@
+#include"my.h"
+int main(int argc,char *argv[])
+{
+int sockfd,recvbytes;
+char buf[MAXDATASIZE];
+struct hostent *host;
+struct sockaddr_in serv_addr;
+if(argc<2){
+fprintf(stderr,"please enter the server's hostname\n");
+exit(1);
+}
+if((host=gethostbyname(argv[1]))==NULL){
+herror("gethostbyname error!");
+exit(1);
+}
+if(sockfd=socket(AF_INET,SOCK_STREAM,0)==-1){
+perror("socket creat error!");
+exit(1);
+}
+serv_addr.sin_family=AF_INET;
+serv_addr.sin_port=htons(SERVPORT);
+serv_addr.sin_addr=*((struct in_addr *)host->h_addr);
+bzero(&(serv_addr.sin_zero),8);
+if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(struct sockaddr))==-1){
+perror("connect errorn");
+exit(1);
+}
+if((recvbytes=recv(sockfd,buf,MAXDATASIZE,0))==-1){
+perror("connect errorn");
+exit(1);
+}
+buf[recvbytes]='\0';
+printf("get:%s",buf);
+close(sockfd);
+}
---
+++ week16/my.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<netdb.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/wait.h>
+#include<errno.h>
+#define SERVPORT 3333
+#define MAXDATASIZE 100
+
